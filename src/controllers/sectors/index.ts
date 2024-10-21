99904265ee060eb6b9c7d3d562d4d04603ddb050
Abdulwaarith <abdulwaarithz@gmail.com>
2024-10-21 08:27:05 +0100

    complete get user API

--- src/controllers/sectors/index.ts
+++ src/controllers/sectors/index.ts
@@ -1,13 +1,13 @@
 import { Request, Response } from "express";
 import { UserModel } from "../../models";
 import { IErrorResponse, IResponseData } from "../types";
-import User from "../../models/user";
+
 
 // Create new user
 export const createCurrentUser = async (
-    req: Request, 
+    req: Request,
     res: Response
-    ): Promise<void> => {
+): Promise<void> => {
     try {
         const { auth0Id } = req.body;
         const existingUser = await UserModel.findOne({ auth0Id });
@@ -42,42 +42,76 @@ export const createCurrentUser = async (
 }
 
 
+// Get current user
+export const getCurrentUser = async (
+    req: Request,
+    res: Response
+): Promise<void> => {
+    try {
+        const currentUser = await UserModel.findOne({ _id: req.userId});
+
+        if (!currentUser) {
+            const result: IErrorResponse = {
+                code: 404,
+                message: "User not found",
+            }
+            res.status(404).json(result);
+            return;
+        }
+
+        const result: IResponseData<typeof currentUser> = {
+            code: 200,
+            data: currentUser,
+            message: "User found successfully",
+        }
+        res.status(200).json(result);
+
+    } catch (error) {
+        const result: IErrorResponse = {
+            code: 500,
+            message: "Internal server error",
+        }
+        res.status(500).json(result);
+    }
+}
+
+
 // Update current user
 export const updateCurrentUser = async (
-    req: Request, 
+    req: Request,
     res: Response): Promise<void> => {
-        try {
-            const { name, addressLine1, country, city } = req.body;
-            const user = await UserModel.findById(req.userId);
+    try {
+        const { name, addressLine1, country, city } = req.body;
+        const user = await UserModel.findById(req.userId);
 
-            if (!user) {
-                const result: IErrorResponse = {
-                    code: 404,
-                    message: "User not found",
-                }
-                res.status(404).json(result);
-                return;
-            }
-
-            user.name = name ?? user.name;
-            user.addressLine1 = addressLine1 ?? user.addressLine1;
-            user.country = country ?? user.country;
-            user.city = city ?? user.city;
-
-            await user.save();
-
-            const result: IResponseData<typeof user> = {
-                code: 200,
-                data: user,
-                message: "User updated successfully",
-            }
-            res.status(200).json(result);
-
-        } catch (error: any) {
+        if (!user) {
             const result: IErrorResponse = {
-                code: 500,
-                message: "Error updating user",
+                code: 404,
+                message: "User not found",
             }
-            res.status(500).json(result);
+            res.status(404).json(result);
+            return;
         }
+
+        user.name = name ?? user.name;
+        user.addressLine1 = addressLine1 ?? user.addressLine1;
+        user.country = country ?? user.country;
+        user.city = city ?? user.city;
+
+        await user.save();
+
+        const result: IResponseData<typeof user> = {
+            code: 200,
+            data: user,
+            message: "User updated successfully",
+        }
+        res.status(200).json(result);
+
+    } catch (error: any) {
+        const result: IErrorResponse = {
+            code: 500,
+            message: "Error updating user",
+        }
+        res.status(500).json(result);
+    }
 }
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,12 +1,18 @@
 import { Router } from "express";
-import { createCurrentUser, updateCurrentUser } from "../controllers";
+import { createCurrentUser, getCurrentUser, updateCurrentUser } from "../controllers";
 import { jwtCheck, jwtParse, validateMyUserRequest } from "../middleware";
 
 
 const router = Router();
 
 /// /api/my/user
+// Get current user
+router.get("./", jwtCheck, jwtParse, getCurrentUser);
+
+// Create a new user
 router.post("/", jwtCheck, createCurrentUser);
+
+// Update current user
 router.put("/",
     jwtCheck,
     jwtParse,
